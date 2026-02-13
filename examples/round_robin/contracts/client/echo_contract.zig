d386c281d72eb412e240346e99cb7b6d46fea07a
Andrej235 <andrej.nenadic06@gmail.com>
2026-02-13 14:47:22 +0100

    Remove /examples/round_robin

--- examples/round_robin/contracts/client/echo_contract.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const znet = @import("znet");
-
-pub const EchoContract = struct {
-    pub fn out(buffer: []const u8) void {
-        std.debug.print("Received {} bytes\n", .{buffer.len});
-    }
-};
--- examples/round_robin/src/client.zig
+++
@@ -1,5 +0,0 @@
-const std = @import("std");
-const znet = @import("znet");
-const EchoContract = @import("server/echo_contract").EchoContract;
-
-pub fn main() !void {}
