0b0f4c745cc2f0daadb5a9e01c66c76ba1843834
Andrej235 <andrej.nenadic06@gmail.com>
2026-02-18 14:00:44 +0100

    Replace async logger's buffer pool with a single ring buffer

--- core/src/logger/async_logger_event_pool.zig
+++ core/src/logger/async_logger_event_pool.zig
@@ -1,19 +1,12 @@
 const std = @import("std");
 
-const buffer_len = 256;
-const buffer_count = 256;
-var buffers: [buffer_count]struct {
-    data: [buffer_len]u8 = undefined,
-    len: usize = 0,
-} = undefined;
+const header_size = @sizeOf(usize);
+const buffer_size = 1024 * 64; // 64kb
+var buffer: [buffer_size]u8 = undefined; // layout: [message size (usize)][message bytes]...
 
-var free_indices: [buffer_count]usize = undefined;
-var free_index_count: usize = 0;
-
-var taken_indices: [buffer_count]usize = undefined;
-var taken_index_count: usize = 0;
-var taken_head: usize = 0;
-var taken_tail: usize = 0;
+var head: usize = 0;
+var tail: usize = 0;
+var free_space: usize = buffer_size;
 
 var mutex: std.Thread.Mutex = .{};
 var cond: std.Thread.Condition = .{};
@@ -21,6 +14,32 @@ var cond: std.Thread.Condition = .{};
 var thread: ?std.Thread = null;
 var state: std.atomic.Value(ShutdownState) = .init(.running);
 var initialized: std.atomic.Value(bool) = .init(false);
+
+const WriteErrors = error{NotEnoughSpace};
+const RingBufferWriter = std.io.GenericWriter(void, WriteErrors, write);
+var writer: RingBufferWriter = .{ .context = {} };
+
+fn write(_: void, bytes: []const u8) WriteErrors!usize {
+    if (bytes.len > free_space) {
+        return WriteErrors.NotEnoughSpace;
+    }
+
+    if (tail + bytes.len < buffer_size) {
+        // no need to wrap around the buffer
+        @memcpy(buffer[tail .. tail + bytes.len], bytes);
+    } else {
+        // need to wrap around the buffer
+        const first_part_size = buffer_size - tail;
+        const second_part_size = bytes.len - first_part_size;
+
+        @memcpy(buffer[tail..], bytes[0..first_part_size]);
+        @memcpy(buffer[0..second_part_size], bytes[first_part_size..]);
+    }
+
+    tail = (tail + bytes.len) % buffer_size;
+    free_space -= bytes.len;
+    return bytes.len;
+}
 
 const ShutdownState = enum(u8) {
     running,
@@ -36,12 +55,7 @@ pub const ShutdownMode = enum {
 pub fn start() !void {
     if (thread != null) return error.ThreadAlreadyStarted;
 
-    for (0..buffer_count) |i| {
-        free_indices[i] = i;
-        taken_indices[i] = i;
-    }
-    free_index_count = buffer_count;
-
+    free_space = buffer_size;
     state.store(.running, .release);
     thread = try std.Thread.spawn(.{}, threadFn, .{});
 
@@ -67,45 +81,79 @@ fn threadFn() void {
     initialized.store(true, .release);
 
     while (true) {
-        const idx = getOldestIdx() orelse break;
-        const message = buffers[idx].data[0..buffers[idx].len];
+        const buffers = getOldestLog() catch break orelse continue;
 
-        var buffer: [64]u8 = undefined;
-        const stderr = std.debug.lockStderrWriter(&buffer);
+        var write_buffer: [64]u8 = undefined;
+        const stderr = std.debug.lockStderrWriter(&write_buffer);
         defer std.debug.unlockStderrWriter();
-        nosuspend stderr.writeAll(message) catch return;
 
-        releaseIdx(idx);
+        nosuspend stderr.writeAll(buffers.first_part) catch return;
+        if (buffers.second_part) |part| {
+            nosuspend stderr.writeAll(part) catch return;
+        }
+
+        // lock to release the buffer space after writing the log message
+        mutex.lock();
+        defer mutex.unlock();
+        const n = header_size + buffers.msg_len;
+        free_space += n;
+        head = (head + n) % buffer_size;
     }
 }
 
-fn getOldestIdx() ?usize {
+fn getOldestLog() !?struct { first_part: []u8, second_part: ?[]u8, msg_len: usize } {
     mutex.lock();
     defer mutex.unlock();
 
-    while (taken_index_count == 0 and state.load(.acquire) == .running) {
+    while (free_space > buffer_size - header_size and state.load(.acquire) == .running) {
         cond.wait(&mutex);
     }
 
     const curr_state = state.load(.acquire);
-    if (curr_state == .immediate or (curr_state == .graceful and taken_index_count == 0)) {
+    if (curr_state == .immediate or (curr_state == .graceful and free_space > buffer_size - header_size)) {
+        return error.ThreadStopped; // no more logs to process, exit the thread
+    }
+
+    const msg_len = len: {
+        if (head + header_size < buffer_size) {
+            const len_bytes = buffer[head..][0..header_size];
+            break :len std.mem.readInt(usize, len_bytes, .big);
+        } else {
+            const first_part_size = buffer_size - head;
+            const second_part_size = header_size - first_part_size;
+
+            var len_bytes: [header_size]u8 = undefined;
+            @memcpy(len_bytes[0..first_part_size], buffer[head..]);
+            @memcpy(len_bytes[first_part_size..], buffer[0..second_part_size]);
+            break :len std.mem.readInt(usize, &len_bytes, .big);
+        }
+    };
+
+    if (free_space > buffer_size - header_size - msg_len) {
+        // missing or corrupted message
         return null;
     }
 
-    // take the oldest index from the taken_indices queue
-    const index = taken_indices[taken_head];
-    taken_head = (taken_head + 1) % taken_indices.len;
-    taken_index_count -= 1;
+    var first_part: []u8 = undefined;
+    var second_part: ?[]u8 = null;
 
-    return index;
-}
+    if (head + header_size < buffer_size - msg_len) { // is there enough memory after the header for the whole message?
+        first_part = buffer[head + header_size .. head + header_size + msg_len];
+    } else if (head + header_size < buffer_size) { // is there at least some memory after the header for the message?
+        const first_part_size = buffer_size - head - header_size;
+        const second_part_size = msg_len - first_part_size;
 
-fn releaseIdx(index: usize) void {
-    mutex.lock();
-    defer mutex.unlock();
+        first_part = buffer[head + header_size .. head + header_size + first_part_size];
+        second_part = buffer[0..second_part_size];
+    } else { // header wrapped or there is exactly 0 bytes after it, read only from the start
+        first_part = buffer[head + header_size - buffer_size .. head + header_size - buffer_size + msg_len];
+    }
 
-    free_indices[free_index_count] = index;
-    free_index_count += 1;
+    return .{
+        .first_part = first_part,
+        .second_part = second_part,
+        .msg_len = msg_len,
+    };
 }
 
 pub fn addLog(
@@ -117,28 +165,26 @@ pub fn addLog(
     mutex.lock();
     defer mutex.unlock();
 
-    if (free_index_count == 0) {
-        // no free buffers, drop the log message
+    if (free_space <= header_size) {
+        // no free space, drop the log message
         return;
     }
 
-    const index = free_indices[free_index_count - 1];
-
     const level_txt = comptime message_level.asText();
-    const prefix2 = if (scope == .default) ": " else "(" ++ @tagName(scope) ++ "): ";
+    const prefix2 = comptime if (scope == .default) ": " else "(" ++ @tagName(scope) ++ "): ";
+    const fmt = comptime level_txt ++ prefix2 ++ format ++ "\n";
 
-    const buffer = &buffers[index];
-    const msg = std.fmt.bufPrint(buffer.data[0..], level_txt ++ prefix2 ++ format ++ "\n", args) catch {
-        // not enough space in the buffer to format the message, drop it
+    const msg_len = std.fmt.count(fmt, args);
+    if (msg_len + header_size > free_space) {
+        // not enough free space, drop the log message
+        return;
+    }
+
+    _ = writer.writeInt(usize, msg_len, .big) catch return;
+    _ = writer.print(fmt, args) catch {
+        // should never happen since we already checked the free space
         return;
     };
 
-    taken_indices[taken_tail] = index;
-    taken_tail = (taken_tail + 1) % taken_indices.len;
-
-    taken_index_count += 1;
-    free_index_count -= 1;
-
-    buffer.len = msg.len;
     cond.signal();
 }
