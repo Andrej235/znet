0196924083d89b57caf6c3230d7777324ad5c66f
Andrej235 <andrej.nenadic06@gmail.com>
2026-04-04 20:56:32 +0200

    Parse http request's method, path, and version

--- core/src/server/parsers/http1.zig
+++ core/src/server/parsers/http1.zig
@@ -6,7 +6,21 @@ const Version = @import("../requests/http.zig").HttpVersion;
 
 const Logger = @import("../../logger/logger.zig").Logger.scoped(.http1_parser);
 
+const HttpState = enum {
+    request_line,
+    headers,
+    body,
+};
+
 pub const Http1Parser = struct {
+    parse_offset: usize,
+
+    method: ?Method = null,
+    version: ?Version = null,
+    path: ?[]const u8 = null,
+
+    state: HttpState = .request_line,
+
     pub fn init() Http1Parser {
         return .{
             .parse_offset = 0,
@@ -14,9 +28,51 @@ pub const Http1Parser = struct {
     }
 
     pub fn parse(self: *Http1Parser, conn: *ConnectionReader) !?ConnectionReader.MessageReadResult {
-        _ = conn;
-        _ = self;
-        Logger.info("Parsing HTTP/1.x message");
+        const buf = conn.current_buffer[self.parse_offset..conn.buffered_bytes];
+        var reader = std.io.Reader.fixed(buf);
+
+        while (reader.takeDelimiterInclusive('\n') catch null) |line| {
+            if (line[line.len - 2] != '\r') {
+                // invalid line ending, http1 requires \r\n
+                Logger.err("Invalid HTTP1 line ending", .{});
+                return error.InvalidHeaders;
+            }
+
+            switch (self.state) {
+                .request_line => {
+                    if (line.len == 2) {
+                        // empty request line
+                        return error.InvalidHeaders;
+                    }
+
+                    var line_reader = std.io.Reader.fixed(line[0 .. line.len - 2]); // exclude \r\n
+                    if (try line_reader.takeDelimiter(' ')) |method| {
+                        if (try line_reader.takeDelimiter(' ')) |path| {
+                            const version = line_reader.buffer[line_reader.seek..];
+
+                            self.state = .headers;
+                            self.method = Method.fromString(method) orelse {
+                                Logger.err("Unsupported HTTP method: {s}", .{method});
+                                // todo: implement arbitrary methods because spec supports them
+                                return error.UnsupportedMethod;
+                            };
+                            self.path = path;
+                            self.version = Version.fromString(version) orelse {
+                                Logger.err("Unsupported HTTP version: {s}", .{version});
+                                return error.UnsupportedVersion;
+                            };
+
+                            Logger.debug("{} {s} ({})", .{ self.method.?, self.path.?, self.version.? });
+                        }
+                    }
+                },
+
+                .headers => {},
+
+                .body => {},
+            }
+        }
+
         return error.Unimplemented;
     }
 };
--- core/src/server/requests/http.zig
+++ core/src/server/requests/http.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const HttpRequest = struct {
     method: HttpMethod,
     path: []const u8,
@@ -14,10 +16,30 @@ pub const HttpMethod = enum {
     CONNECT,
     TRACE,
     PATCH,
+
+    pub fn fromString(s: []const u8) ?HttpMethod {
+        if (std.mem.eql(u8, s, "GET")) return .GET;
+        if (std.mem.eql(u8, s, "POST")) return .POST;
+        if (std.mem.eql(u8, s, "PUT")) return .PUT;
+        if (std.mem.eql(u8, s, "DELETE")) return .DELETE;
+        if (std.mem.eql(u8, s, "HEAD")) return .HEAD;
+        if (std.mem.eql(u8, s, "OPTIONS")) return .OPTIONS;
+        if (std.mem.eql(u8, s, "CONNECT")) return .CONNECT;
+        if (std.mem.eql(u8, s, "TRACE")) return .TRACE;
+        if (std.mem.eql(u8, s, "PATCH")) return .PATCH;
+        return null;
+    }
 };
 
 pub const HttpVersion = enum {
     Http10,
     Http11,
     Http2,
+
+    pub fn fromString(s: []const u8) ?HttpVersion {
+        if (std.mem.eql(u8, s, "HTTP/1.0")) return .Http10;
+        if (std.mem.eql(u8, s, "HTTP/1.1")) return .Http11;
+        if (std.mem.eql(u8, s, "HTTP/2.0")) return .Http2;
+        return null;
+    }
 };
